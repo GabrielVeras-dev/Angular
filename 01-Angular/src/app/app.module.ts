a431b3a059d0310033f260f5d894ee6ec6e39a87
GabrielVeras-dev <bielveras.e@hotmail.com>
2022-08-11 17:38:40 -0300

    Criado novo component, importado component e adicionado modulo FontAwesome

--- 01-Angular/src/app/app.module.ts
+++ 01-Angular/src/app/app.module.ts
@@ -3,15 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { StarComponent } from './star/star.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
+    StarComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
